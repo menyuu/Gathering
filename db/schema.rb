46bf8849e1928a6c59a5ea1d3527420f94ff1d00
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-26 08:15:35 +0000

    [Add]Genreモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_26_080827) do
+ActiveRecord::Schema.define(version: 2022_07_26_081027) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 2022_07_26_080827) do
   end
 
   create_table "games", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
