6506fe636403874beadcf5b1ae6c3534bbc86cff
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-26 08:22:46 +0000

    [Add]Favoriteモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_26_081812) do
+ActiveRecord::Schema.define(version: 2022_07_26_082154) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 2022_07_26_081812) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "end_user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["end_user_id"], name: "index_favorites_on_end_user_id"
+    t.index ["post_id"], name: "index_favorites_on_post_id"
   end
 
   create_table "games", force: :cascade do |t|
@@ -89,6 +98,8 @@ ActiveRecord::Schema.define(version: 2022_07_26_081812) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "favorites", "end_users"
+  add_foreign_key "favorites", "posts"
   add_foreign_key "groups", "owners"
   add_foreign_key "posts", "end_users"
   add_foreign_key "relationships", "followeds"
