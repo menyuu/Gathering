721d655972ff0a42c83a7b83c1cc99fc067fda5e
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-26 11:24:07 +0000

    [Add]devise用コントローラ・ビューの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :end_users
+  devise_for :end_users, skip: [:password], controllers: {
+    registrations: "end_user/registrations",
+    sessions: "end_user/sessions"
+  }
+
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    session: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
