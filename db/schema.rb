d37e1e5cfbab363b3753de02d8346e6575710d79
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-26 07:54:26 +0000

    [Add]Postモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_26_073650) do
+ActiveRecord::Schema.define(version: 2022_07_26_075001) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,4 +39,13 @@ ActiveRecord::Schema.define(version: 2022_07_26_073650) do
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.integer "end_user_id", null: false
+    t.text "post", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["end_user_id"], name: "index_posts_on_end_user_id"
+  end
+
+  add_foreign_key "posts", "end_users"
 end
