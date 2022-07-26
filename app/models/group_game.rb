c7eb951fce3ccb42a41d91228b4e1bacf8ebec6e
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-26 10:35:37 +0000

    [Add]GroupGameモデル

---
+++ app/models/group_game.rb
@@ -0,0 +1,4 @@
+class GroupGame < ApplicationRecord
+  belongs_to :group
+  belongs_to :genre
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_26_103004) do
+ActiveRecord::Schema.define(version: 2022_07_26_103346) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -106,6 +106,24 @@ ActiveRecord::Schema.define(version: 2022_07_26_103004) do
     t.index ["group_id"], name: "index_group_chats_on_group_id"
   end
 
+  create_table "group_games", force: :cascade do |t|
+    t.integer "group_id", null: false
+    t.integer "genre_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["genre_id"], name: "index_group_games_on_genre_id"
+    t.index ["group_id"], name: "index_group_games_on_group_id"
+  end
+
+  create_table "group_tags", force: :cascade do |t|
+    t.integer "group_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_group_tags_on_group_id"
+    t.index ["tag_id"], name: "index_group_tags_on_tag_id"
+  end
+
   create_table "groups", force: :cascade do |t|
     t.integer "owner_id", null: false
     t.string "name", null: false
@@ -175,6 +193,10 @@ ActiveRecord::Schema.define(version: 2022_07_26_103004) do
   add_foreign_key "favorites", "posts"
   add_foreign_key "group_chats", "end_users"
   add_foreign_key "group_chats", "groups"
+  add_foreign_key "group_games", "genres"
+  add_foreign_key "group_games", "groups"
+  add_foreign_key "group_tags", "groups"
+  add_foreign_key "group_tags", "tags"
   add_foreign_key "groups", "owners"
   add_foreign_key "post_comments", "end_users"
   add_foreign_key "post_comments", "posts"
