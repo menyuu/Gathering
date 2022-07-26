62d6f2d91b6a7e813307346708db0c756e692370
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-26 08:08:00 +0000

    [Add]Tagモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_26_080222) do
+ActiveRecord::Schema.define(version: 2022_07_26_080658) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -54,6 +54,12 @@ ActiveRecord::Schema.define(version: 2022_07_26_080222) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["end_user_id"], name: "index_posts_on_end_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "groups", "owners"
