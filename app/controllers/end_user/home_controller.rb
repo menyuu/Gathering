20911fa93eba7920b746ce86137c5fbc2338e079
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-31 08:38:58 +0000

    [Add]topページの自動リロード機能実装

--- app/controllers/end_user/home_controller.rb
+++ app/controllers/end_user/home_controller.rb
@@ -2,9 +2,9 @@ class EndUser::HomeController < ApplicationController
   def top
     @user = EndUser.new
     @posts = Post.with_attached_images.limit(5).order(created_at: :DESC)
-    respond_to do |format|
-      format.js
-      format.html
-    end
+    # respond_to do |format|
+    #   format.js
+    #   format.html
+    # end
   end
 end
--- app/javascript/packs/test.js
+++ app/javascript/packs/test.js
@@ -1,10 +1,14 @@
-const milliseconds = 60000
-const sync = () => {
-  $.ajax({
-    type: 'GET',
-    url: $(location).attr('href'),
-    dataType: 'script'
-  })
-}
-const interval = setInterval(sync, milliseconds)
-$(document).on('turbolinks:before-cache turbolinks:before-render', () => clearTimeout(interval))
+/* global $*/
+/* global location $*/
+$(document).on('turbolinks:load', function() {
+  const milliseconds = 10000;
+  const sync = function() {
+    $.ajax({
+      type: 'GET',
+      url: $(location).attr('href'),
+      dataType: 'script'
+    });
+  };
+  const interval = setInterval(sync, milliseconds);
+$(document).on('turbolinks:before-cache turbolinks:before-render', () => clearTimeout(interval));
+});
