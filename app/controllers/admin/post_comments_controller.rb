272be8b75962f596ecb034eb52411c9ebd7a9bee
menyuu <101458830+menyuu@users.noreply.github.com>
2022-08-07 22:40:37 +0000

    [Update]flashメッセージの設定

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,6 +1,6 @@
 class Admin::PostCommentsController < ApplicationController
   def destroy
     PostComment.find(params[:id]).destroy
-    redirect_to admin_post_path(params[:post_id]), notice: "ユーザーの投稿が正常に削除されました。"
+    redirect_to admin_post_path(params[:post_id]), notice: "ユーザーのコメントが正常に削除されました。"
   end
 end
