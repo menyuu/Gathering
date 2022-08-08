bcda7a3149c24edd20c6e8f29807acb6318b22c5
menyuu <101458830+menyuu@users.noreply.github.com>
2022-08-08 18:44:12 +0000

    [Update]インデント修正

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,6 +1,6 @@
  /*global $*/
 $(document).on('turbolinks:load', function(){
-   $(function() {
-      $('.flash').fadeOut(5000);
-    });
+  $(function() {
+    $('.flash').fadeOut(5000);
   });
+});
