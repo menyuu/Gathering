b80f5ccc74b0ea6a381e0bdc2dfb92e35befc284
menyuu <101458830+menyuu@users.noreply.github.com>
2022-08-10 16:05:00 +0000

    [Add]管理者側のタグページの作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,36 @@
 class Admin::GenresController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :admin_display_show_genres
+
   def index
+    @tag = Tag.new
   end
 
-  def edit
+  def create
+    @tag = Tag.find_or_initialize_by(tag_params)
+    @tag.save
+    @tag.update(status: "hide")
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    @tag.update(status: params[:status])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+
+  def admin_display_show_tags
+    @prepared_tags = Tag.where(status: "prepared").order(updated_at: :DESC)
+    @self_made_tags = Tag.where(status: "self_made").order(updated_at: :DESC)
+    @hide_tags = Tag.where(status: "hide").order(updated_at: :DESC)
   end
 end
--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,37 +1,39 @@
 class Admin::TagsController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :admin_display_show_tags
+
   def index
-    @prepared_tags = Tag.where(status: "prepared").order(created_at: :DESC)
-    @self_made_tags = Tag.where(status: "self_made").order(created_at: :DESC)
-    @hide_tags =Tag.where(status: "hide").order(updated_at: :DESC)
     @tag = Tag.new
   end
 
   def create
-    @tag = Tag.find_or_create_by(tag_params)
-    @prepared_tags = Tag.where(status: "prepared").order(created_at: :DESC)
-    @self_made_tags = Tag.where(status: "self_made").order(created_at: :DESC)
-    @hide_tags =Tag.where(status: "hide").order(updated_at: :DESC)
+    @tag = Tag.find_or_initialize_by(tag_params)
+    @tag.save
+    @tag.update(status: "hide")
+    redirect_to request.referer, notice: "タグを作成しました。"
   end
 
   def update
     @tag = Tag.find(params[:id])
     @tag.update(status: params[:status])
-    @prepared_tags = Tag.where(status: "prepared").order(created_at: :DESC)
-    @self_made_tags = Tag.where(status: "self_made").order(created_at: :DESC)
-    @hide_tags =Tag.where(status: "hide").order(updated_at: :DESC)
+    flash[:notice] = "タグの情報を更新しました。"
   end
 
   def destroy
     @tag = Tag.find(params[:id])
     @tag.destroy
-    @prepared_tags = Tag.where(status: "prepared").order(created_at: :DESC)
-    @self_made_tags = Tag.where(status: "self_made").order(created_at: :DESC)
-    @hide_tags =Tag.where(status: "hide").order(updated_at: :DESC)
+    flash[:notice] = "タグを削除しました。"
   end
 
   private
 
   def tag_params
-    params.require(:tag).permit(:name, :status)
+    params.require(:tag).permit(:name)
+  end
+
+  def admin_display_show_tags
+    @prepared_tags = Tag.where(status: "prepared").order(updated_at: :DESC)
+    @self_made_tags = Tag.where(status: "self_made").order(updated_at: :DESC)
+    @hide_tags = Tag.where(status: "hide").order(updated_at: :DESC)
   end
 end
