e512f77d6e850c2d1df4d5d159c2f4e56f033b04
menyuu <101458830+menyuu@users.noreply.github.com>
2022-08-12 14:07:52 +0000

    [Update]制限の追加/レイアウト調整

--- app/controllers/end_user/home_controller.rb
+++ app/controllers/end_user/home_controller.rb
@@ -1,6 +1,15 @@
 class EndUser::HomeController < ApplicationController
+  before_action :user_after_signed_in_prohibit
+
   def top
     @user = EndUser.new
     @posts = Post.where(status: "published").with_attached_images.includes(:post_tags, :tags, user: [icon_attachment: [:blob]]).where(user: {status: "published"}).limit(5).order(created_at: :DESC)
+  end
+
+  def user_after_signed_in_prohibit
+    if current_end_user
+      flash[:notice] = "すでにログインしています。"
+      # リダイレクト先を作る
+    end
   end
 end
