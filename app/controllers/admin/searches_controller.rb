7c2f962ba70a6d29549acf5284c9c3c694a754d7
menyuu <101458830+menyuu@users.noreply.github.com>
2022-08-18 02:06:11 +0000

    [Update]リンク先の変更

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,14 +1,26 @@
 class Admin::SearchesController < ApplicationController
   def search
+    @content = params[:content]
     @model = params[:model]
     @word = params[:word]
-    case @model
-    when "user"
-      @search_result = EndUser.where(id: @word)
-    when "post"
-      @search_result = Post.where(id: @word)
-    when "group"
-      @search_result = Group.where(id: @word)
+    if @content == "id"
+      case @model
+      when "user"
+        @search_result = EndUser.where(id: @word)
+      when "post"
+        @search_result = Post.where(id: @word)
+      when "group"
+        @search_result = Group.where(id: @word)
+      end
+    else
+      case @model
+      when "user"
+        @search_result = EndUser.where(name: @word)
+      when "post"
+        @search_result = Post.where(text: @word)
+      when "group"
+        @search_result = Group.where(name: @word)
+      end
     end
   end
 end
