1c6125ccd3efa48aaa0b28d32756542620d4b2b9
menyuu <101458830+menyuu@users.noreply.github.com>
2022-08-18 02:21:36 +0000

    [Update]部分テンプレートの作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,12 +12,12 @@ class Admin::UsersController < ApplicationController
   def update
     @user = EndUser.find(params[:id])
     @user.update(status: "published")
-    redirect_to request.referer
+    redirect_to request.referer, notice: "該当のユーザーのアカウントを復旧しました。"
   end
 
   def destroy
     @user = EndUser.find(params[:id])
     @user.update(status: "freeze")
-    redirect_to request.referer
+    redirect_to request.referer, alert: "該当のユーザーのアカウント停止をしました。"
   end
 end
