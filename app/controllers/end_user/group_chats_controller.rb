73d768fd3133af6c2ea667b0277826d782675418
menyuu <101458830+menyuu@users.noreply.github.com>
2022-08-22 20:55:14 +0000

    [Update]グループチャットの記述の統一

--- app/controllers/end_user/group_chats_controller.rb
+++ app/controllers/end_user/group_chats_controller.rb
@@ -1,34 +1,26 @@
 class EndUser::GroupChatsController < ApplicationController
   before_action :authenticate_end_user!
   before_action :ensure_correct_user
+  before_action :common_group_chat
   # before_action :forbid_guestuser, only: [:create, :destroy]
 
   def index
-    @group = Group.find(params[:group_id])
     @group_chat = GroupChat.new
     @members = @group.users.page(params[:page]).without_count.per(1).order(name: :ASC)
-    @chats = @group.group_chats.includes(user: [icon_attachment: [:blob]])
   end
 
-  # javascriptと非同期を同時にする方法を模索する
   def create
-    @group = Group.find(params[:group_id])
     @group_chat = current_end_user.group_chats.new(chat_params)
     @group_chat.group_id = @group.id
-    @members = @group.users.page(params[:page]).without_count.per(1).order(name: :ASC)
     if @group_chat.save
-      @chats = @group.group_chats
     else
       render :error
     end
   end
 
   def destroy
-    @group = Group.find(params[:group_id])
-    @group_chat = GroupChat.new
     @destroy_group_chat = GroupChat.find(params[:id])
     @destroy_group_chat.destroy
-    @chats = @group.group_chats.includes(user: [icon_attachment: [:blob]])
   end
 
   private
@@ -42,5 +34,10 @@ class EndUser::GroupChatsController < ApplicationController
     unless group.includesUser?(current_end_user)
       redirect_to group_path(group), alert: "グループのメンバーではありません。"
     end
+  end
+
+  def common_group_chat
+    @group = Group.find(params[:group_id])
+    @chats = @group.group_chats.includes(user: [icon_attachment: [:blob]])
   end
 end
