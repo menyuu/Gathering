22aa4d2cad3b176ce5d1ebf92832666d75552803
menyuu <101458830+menyuu@users.noreply.github.com>
2022-08-22 21:30:55 +0000

    [Update]タグ画面のレイアウト調整

--- app/javascript/packs/chat.js
+++ app/javascript/packs/chat.js
@@ -1,7 +1,9 @@
 /* global $ */
 $(document).on('turbolinks:load', function() {
     window.onload = function() {
-    const scrollHeight = document.getElementById("group-chat-area").scrollHeight;
-    document.getElementById("group-chat-area").scrollTop = scrollHeight;
+    if (document.getElementById("group-chat-area")) {
+      const scrollHeight = document.getElementById("group-chat-area").scrollHeight;
+      document.getElementById("group-chat-area").scrollTop = scrollHeight;
+    }
   };
 });
