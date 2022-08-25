91c5bcaeb7fbd4682f7026d59c4b6f41165124cf
menyuu <101458830+menyuu@users.noreply.github.com>
2022-08-25 07:01:11 +0000

    [Update]記述の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
 
   def freeze_user
-    if end_user_signed_id? && current_end_user.freeze?
+    if end_user_signed_in? && current_end_user.freeze?
       sign_out current_end_user
       redirect_to root_path, alert: "アカウントが見つかりません。"
     end
