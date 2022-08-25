6a33c2087133d5290409aa0cbcad1f2e06169501
menyuu <101458830+menyuu@users.noreply.github.com>
2022-08-25 06:21:36 +0000

    [Update]ログアウト処理の追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -26,6 +26,7 @@ class Admin::UsersController < ApplicationController
   def freeze
     @user = EndUser.find(params[:id])
     @user.update(status: "freeze")
+    sign_out @user
     redirect_to request.referer, alert: "該当のユーザーのアカウント停止にしました。"
   end
 end
