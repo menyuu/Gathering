aa3e3bcea815d8197db5efe3d2e83660269caaf8
menyuu <101458830+menyuu@users.noreply.github.com>
2022-08-27 04:38:10 +0000

    [Update]トップページのレスポンシブ対応

--- app/javascript/packs/top.js
+++ app/javascript/packs/top.js
@@ -1,12 +1,16 @@
 /* global $*/
 /* global location */
-  const milliseconds = 300000;
-  const sync = function() {
-    $.ajax({
-      type: 'GET',
-      url: $(location).attr('href'),
-      dataType: 'script'
-    });
-  };
-  const interval = setInterval(sync, milliseconds);
-  $(document).on('turbolinks:before-cache turbolinks:before-render', () => clearTimeout(interval));
+
+// 5分に設定
+const milliseconds = 300000;
+// ajax処理を行う
+const sync = function() {
+  $.ajax({
+    type: 'GET',
+    url: $(location).attr('href'),
+    dataType: 'script'
+  });
+};
+const interval = setInterval(sync, milliseconds);
+// 5分おきにajax処理を行ってトップページを更新する
+$(document).on('turbolinks:before-cache turbolinks:before-render', () => clearTimeout(interval));
