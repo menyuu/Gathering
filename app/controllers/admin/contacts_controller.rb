d5644e8fdb84335386405eef14b5611439a070c8
menyuu <101458830+menyuu@users.noreply.github.com>
2022-09-13 04:01:42 +0000

    [Add]管理者側のお問い合わせ確認の追加

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,7 +1,24 @@
 class Admin::ContactsController < ApplicationController
   def index
+    @contacts = Contact.page(params[:page]).without_count.per(5)
   end
 
   def show
+    @contact = Contact.find(params[:id])
+    if @contact.unread?
+      @contact.update(status: "read")
+    end
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    @contact.update!(contact_params)
+    redirect_to request.referer
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:status)
   end
 end
