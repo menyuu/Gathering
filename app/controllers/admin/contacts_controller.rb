eca99fef464bcf8198049218d2983ca3cdb55712
menyuu <101458830+menyuu@users.noreply.github.com>
2022-09-14 02:15:36 +0000

    [Update]管理者 問い合わせソート機能追加

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,6 +1,18 @@
 class Admin::ContactsController < ApplicationController
   def index
     @contacts = Contact.page(params[:page]).without_count.per(5)
+    case params[:contact_search]
+    when "unread"
+      @contacts = Contact.where(status: "unread").page(params[:page]).without_count.per(5)
+    when "read"
+      @contacts = Contact.where(status: "read").page(params[:page]).without_count.per(5)
+    when "hold"
+      @contacts = Contact.where(status: "hold").page(params[:page]).without_count.per(5)
+    when "working"
+      @contacts = Contact.where(status: "working").page(params[:page]).without_count.per(5)
+    when "resolved"
+      @contacts = Contact.where(status: "resolved").page(params[:page]).without_count.per(5)
+    end
   end
 
   def show
