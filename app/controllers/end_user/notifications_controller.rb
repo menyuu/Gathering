e4f3115925c1217bcfab4116d648fd5c06108d7f
menyuu <101458830+menyuu@users.noreply.github.com>
2022-09-15 02:36:01 +0000

    [Update]通知機能のレイアウト調整

--- app/controllers/end_user/notifications_controller.rb
+++ app/controllers/end_user/notifications_controller.rb
@@ -1,5 +1,9 @@
 class EndUser::NotificationsController < ApplicationController
   def update
-    current_end_user.passive_notifications.update_all(checked: true)
+    current_end_user.passive_notifications.each do |notification|
+      if notification.checked == false
+        notification.update(checked: true)
+      end
+    end
   end
 end
